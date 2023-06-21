6d56cd4027d546e29885aa7980bb97b519ba4b1d
Chifan <itzirfanmt@gmail.com>
2023-06-21 19:21:57 +0700

    I committed this to memory

---
+++ index.js
@@ -0,0 +1,28 @@
+let passwordLength = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+
+function generatePassword(len) {
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numeric = "0123456789"
+    const symbol = "@_"
+
+    const data = lowerAlphabet + upperAlphabet + numeric + symbol
+    let generator = '';
+    for (let index = 0; index < len; index++) {
+        generator += data[Math.floor(Math.random() * data.length)];
+    }
+    return generator
+}
+
+function getPassword() {
+    const newPassword = generatePassword(passwordLength.value)
+    password.value = newPassword
+    setTimeout(() => {
+        alert('password has been generated!')
+    }, 500) 
+}
+
+function savePassword() {
+ 
+}
